f5e10795ebc96b2c4e93b7858fdf6d23d22e1d24
TheThief <positronthief@gmail.com>
2026-01-25 10:57:26 +0000

    Day 12

---
+++ AoC_2025_Day12/AoC_2025_Day12.h
@@ -0,0 +1,97 @@
+﻿#pragma once
+
+import std;
+
+#include "common/stream_line.h"
+#include "common/delimitered.h"
+
+using namespace std::string_view_literals;
+
+namespace aoc2025::day12
+{
+	struct present
+	{
+		std::array<std::string, 3> shape;
+
+		friend std::istream& operator>>(std::istream& is, present& p)
+		{
+			int id;
+			is >> id >> required{':'};
+			for (auto& line : p.shape)
+			{
+				is >> line;
+			}
+			return is;
+		}
+	};
+	struct tree
+	{
+		std::pair<int, int> size;
+		std::vector<int> present_counts;
+
+		friend std::istream& operator>>(std::istream& is, tree& t)
+		{
+			delimitered_pair<int, int, 'x'> size;
+			line<std::vector<int>> present_counts;
+			is >> size >> required{ ':' } >> present_counts;
+			t.size = size;
+			t.present_counts = present_counts;
+			return is;
+		}
+	};
+	struct puzzle_input
+	{
+		std::vector<present> presents;
+		std::vector<tree> trees;
+
+		friend std::istream& operator>>(std::istream& is, puzzle_input& input)
+		{
+			// istream_view when used with take still extracts one too many items
+			// std::ranges::istream_view<present>(is) | std::views::take(6) | std::ranges::to<std::vector<present>>();
+			input.presents = std::views::iota(0,6) | std::views::transform(
+				[&](int)
+				{
+					present p;
+					is >> p;
+					return p;
+				}) | std::ranges::to<std::vector<present>>();
+			input.trees = std::ranges::istream_view<tree>(is) | std::ranges::to<std::vector<tree>>();
+			return is;
+		}
+	};
+
+	std::int64_t part1(const puzzle_input& input)
+	{
+		return std::ranges::count_if(input.trees,
+			[&](const tree& t)
+			{
+				int num_presents = std::ranges::fold_left(t.present_counts, 0, std::plus{});
+				int trivial_fit = (t.size.first / 3) * (t.size.second / 3);
+				if (num_presents <= trivial_fit)
+					return true;
+
+				int total_squares = std::ranges::fold_left(
+					std::views::zip_transform(
+						[](const present& present, int count)
+						{
+							return std::ranges::fold_left(present.shape |
+								std::views::transform(
+									[](const std::string& line) {
+										return (int)std::ranges::count(line, '#');
+									}),
+								0, std::plus{}) * count;
+						},
+						input.presents, t.present_counts),
+					0, std::plus{});
+				if (total_squares > t.size.first * t.size.second)
+					return false;
+
+				return true; // todo - solve NP-hard packing problem
+			});
+	}
+
+	std::int64_t part2(const puzzle_input& input)
+	{
+		return 0;
+	}
+}
---
+++ AoC_2025_Day12_Test/AoC_2025_Day12_Test.cpp
@@ -0,0 +1,78 @@
+﻿#include "CppUnitTest.h"
+
+#include "AoC_2025_Day12/AoC_2025_Day12.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std::string_literals;
+
+namespace aoc2025::day12::test
+{
+	const std::string_view test_input_1 =
+R"(0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2)"sv;
+
+	const std::string_view test_input_2 = test_input_1;
+
+	const int64_t expected_test_ouput_1 = 2;
+	const int64_t expected_test_ouput_2 = 2;
+
+	TEST_CLASS(Test)
+	{
+	public:
+		Test()
+		{
+		}
+
+		TEST_METHOD(Part1)
+		{
+			puzzle_input input;
+			std::ispanstream(test_input_1) >> input;
+			Assert::AreEqual(expected_test_ouput_1, part1(input));
+		}
+
+		TEST_METHOD(Part2)
+		{
+			puzzle_input input;
+			std::ispanstream(test_input_2) >> input;
+			Assert::AreEqual(expected_test_ouput_2, part2(input));
+		}
+	};
+}
+
+
+
+
+
+
+
